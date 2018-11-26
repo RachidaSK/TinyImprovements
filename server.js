515123ff676ca52a478f59fb66c7f43900930afa
RachidaSK <rachida.koco@gmail.com>
2018-11-25 22:38:06 -0500

    Configure mLab connection

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const request = require("request");
+const cheerio = require("cheerio");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+// app.use(logger("dev"));
+// app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/kudosDB", {useNewUrlParser: true});
+
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect("mongodb://localhost/kudosDB", {useNewUrlParser: true});
+}
+
+const db = mongoose.connection;
+db.on("error", function(err) {
+    console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+console.log("Mongoose connection successful")
+})
 
 require("./routes/api-routes")(app);
 
